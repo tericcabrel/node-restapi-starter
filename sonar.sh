b55a427bedeae6ab2e60bd746e31612a756775c7
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2020-04-21 13:50:33 +0100

    Sonarqube integration

---
+++ sonar.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sonar-scanner \
+  -Dsonar.projectKey=node-rest-api-starter \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.login=$1 \
+  -Dsonar.typescript.tsconfigPath=tsconfig.json \
+  -Dsonar.inclusions="app/**/*" \
+  -Dsonar.exclusions="app/tests/**/*, app/core/mailer/templates/**/*" \
+
+# Pass the token of the project as the first argument when executing this file
+# Example:
+# ./sonar.sh 3660a926a83a7c2f37496d11828e21ea73d34468
